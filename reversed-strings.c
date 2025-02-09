6f5b918399beec53cfd826bc126af554b13156fb
Nepali-Keto_UwU <nepaliketo.uwu@gmail.com>
2025-02-09 10:11:51 +0000

    reversed-strings.c

--- reversed-strings.c
+++ reversed-strings.c
@@ -1,3 +1,8 @@
+/* DESCRIPTION: 
+Complete the solution so that it reverses the string passed into it. 
+'world'  =>  'dlrow'
+'word'   =>  'drow' */
+// My solution:
 #include<string.h>
 
 char *strrev (char *string)
